af5efe1d17278578e4038bd07ee04b0190502668
Veerle Ledoux <veerle.ledoux@skyline.be>
2026-02-26 10:11:49 +0100

    Creation of elements

--- Empower 2026 - AI Operational Monitoring - Part 1/ElementInstaller.cs
+++ Empower 2026 - AI Operational Monitoring - Part 1/ElementInstaller.cs
@@ -1,155 +1,181 @@
 ﻿namespace Elements
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Linq;
-	using System.Threading;
-	using Skyline.DataMiner.Analytics.DataTypes;
-	using Skyline.DataMiner.Automation;
-	using Skyline.DataMiner.Core.DataMinerSystem.Automation;
-	using Skyline.DataMiner.Core.DataMinerSystem.Common;
-	using Skyline.DataMiner.Net.Messages;
-	using Skyline.DataMiner.Net.Messages.Advanced;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using Skyline.DataMiner.Analytics.DataTypes;
+    using Skyline.DataMiner.Automation;
+    using Skyline.DataMiner.Core.DataMinerSystem.Automation;
+    using Skyline.DataMiner.Core.DataMinerSystem.Common;
+    using Skyline.DataMiner.Net;
+    using Skyline.DataMiner.Net.Messages;
+    using Skyline.DataMiner.Net.Messages.Advanced;
 
-	internal class ElementInstaller
-	{
-		private readonly IEngine engine;
+    internal class ElementInstaller
+    {
+        private readonly IEngine engine;
 
-		public ElementInstaller(IEngine engine)
-		{
-			this.engine = engine;
-		}
+        public ElementInstaller(IEngine engine)
+        {
+            this.engine = engine;
+        }
 
-		public void InstallDefaultContent()
-		{
-			int viewID = CreateViews(new string[] { "DataMiner Catalog", "Empower2026"});
-			CreateElement($"RAD - Commtia LON 1", "AI - Commtia DAB", "1.0.0.1-fast", viewID, "TrendTemplate_PA_Demo", "AlarmTemplate_PA_Demo");
-		}
+        public void InstallDefaultContent()
+        {
+            int viewID = CreateViews(new string[] { "DataMiner Catalog", "Empower 2026", "AI Operational Monitoring", "Behavioral Anomaly Detection Demo" });
+            CreateElement("Empower 2026 - AI - Audio bit rate", "Empower 2026 - AI - Audio bit rate CBR-VBR", "0.0.0.1", viewID);
+            CreateElement("Empower 2026 - AI - Task Manager", "Empower 2026 - AI - Task Manager", "0.0.0.1", viewID);
 
-		private void AssignVisioToView(int viewID, string visioFileName)
-		{
-			var request = new AssignVisualToViewRequestMessage(viewID, new Skyline.DataMiner.Net.VisualID(visioFileName));
+            viewID = CreateViews(new string[] { "DataMiner Catalog", "Empower 2026", "AI Operational Monitoring", "Pattern Matching Demo" });
 
-			engine.SendSLNetMessage(request);
-		}
+            viewID = CreateViews(new string[] { "DataMiner Catalog", "Empower 2026", "AI Operational Monitoring", "Proactive Detection Demo" });
+            CreateElement("Empower 2026 - AI - SFP Monitor", "Empower 2026 - AI - SFP", "0.0.0.1", viewID);
+            CreateElement("Empower 2026 - AI - AMS Server", "Empower 2026 - AI - AMS Server", "0.0.0.3", viewID);
+            
+            int CMSViewID = CreateViews(new string[] { "DataMiner Catalog", "Empower 2026","AI Operational Monitoring", "Proactive Detection Demo",
+                "Content Management Servers" });
+            CreateElement("Empower 2026 - AI - CMS 1", "Empower 2026 - AI - Content Management Server 1", "0.0.0.1", CMSViewID);
+            CreateElement("Empower 2026 - AI - CMS 2", "Empower 2026 - AI - Content Management Server 2", "0.0.0.1", CMSViewID);
+            CreateElement("Empower 2026 - AI - CMS 3", "Empower 2026 - AI - Content Management Server 3", "0.0.0.1", CMSViewID);
+            AssignVisioToView(CMSViewID, "Empower 2025 - AI - Content Management Server.vsdx");
 
-		private int? GetView(string viewName)
-		{
-			var views = engine.SendSLNetMessage(new GetInfoMessage(InfoType.ViewInfo));
-			foreach (var m in views)
-			{
-				var viewInfo = m as ViewInfoEventMessage;
-				if (viewInfo == null)
-					continue;
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(30));
+            var element = engine.FindElement("Empower 2026 - AI - Audio bit rate");
+            if (element != null)
+                element.SetParameter(102, 1);
 
-				if (viewInfo.Name == viewName)
-					return viewInfo.ID;
-			}
+            viewID = CreateViews(new string[] { "DataMiner Catalog", "Empower 2026", "AI Operational Monitoring", "Relational Anomaly Detection Demo" });
+            CreateElement($"RAD - Commtia LON 1", "AI - Commtia DAB", "1.0.0.1", viewID, "TrendTemplate_PA_Demo", "AlarmTemplate_PA_Demo");
+            Thread.Sleep(5000);
+            CreateElement($"RAD - Commtia LON 2", "AI - Commtia DAB", "1.0.0.1", viewID, "TrendTemplate_PA_Demo", "AlarmTemplate_PA_Demo");
+            Thread.Sleep(5000);            
+        }
 
-			return null;
-		}
+        private void AssignVisioToView(int viewID, string visioFileName)
+        {
+            var request = new AssignVisualToViewRequestMessage(viewID, new Skyline.DataMiner.Net.VisualID(visioFileName));
 
-		private int CreateNewView(string viewName, string parentViewName)
-		{
-			var request = new SetDataMinerInfoMessage
-			{
-				bInfo1 = int.MaxValue,
-				bInfo2 = int.MaxValue,
-				DataMinerID = -1,
-				HostingDataMinerID = -1,
-				IInfo1 = int.MaxValue,
-				IInfo2 = int.MaxValue,
-				Sa1 = new SA(new string[] { viewName, parentViewName }),
-				What = (int)NotifyType.NT_ADD_VIEW_PARENT_AS_NAME,
-			};
+            engine.SendSLNetMessage(request);
+        }
 
-			var response = engine.SendSLNetSingleResponseMessage(request);
-			if (!(response is SetDataMinerInfoResponseMessage infoResponse))
-				throw new ArgumentException("Unexpected message returned by DataMiner");
+        private int? GetView(string viewName)
+        {
+            var views = engine.SendSLNetMessage(new GetInfoMessage(InfoType.ViewInfo));
+            foreach (var m in views)
+            {
+                var viewInfo = m as ViewInfoEventMessage;
+                if (viewInfo == null)
+                    continue;
 
-			return infoResponse.iRet;
-		}
+                if (viewInfo.Name == viewName)
+                    return viewInfo.ID;
+            }
 
-		private int CreateViews(string[] viewNames)
-		{
-			int? firstNonExistingViewLevel = null;
-			int? lastExistingViewID = null;
-			string lastExistingViewName = null;
+            return null;
+        }
 
-			for (int i = viewNames.Length - 1; i >= 0; --i)
-			{
-				int? viewID = GetView(viewNames[i]);
-				if (viewID.HasValue)
-				{
-					lastExistingViewID = viewID;
-					lastExistingViewName = viewNames[i];
-					firstNonExistingViewLevel = i + 1;
-					break;
-				}
-			}
+        private int CreateNewView(string viewName, string parentViewName)
+        {
+            var request = new SetDataMinerInfoMessage
+            {
+                bInfo1 = int.MaxValue,
+                bInfo2 = int.MaxValue,
+                DataMinerID = -1,
+                HostingDataMinerID = -1,
+                IInfo1 = int.MaxValue,
+                IInfo2 = int.MaxValue,
+                Sa1 = new SA(new string[] { viewName, parentViewName }),
+                What = (int)NotifyType.NT_ADD_VIEW_PARENT_AS_NAME,
+            };
 
-			if (firstNonExistingViewLevel.HasValue && firstNonExistingViewLevel == viewNames.Length)
-				return lastExistingViewID.Value;
+            var response = engine.SendSLNetSingleResponseMessage(request);
+            if (!(response is SetDataMinerInfoResponseMessage infoResponse))
+                throw new ArgumentException("Unexpected message returned by DataMiner");
 
-			if (!firstNonExistingViewLevel.HasValue)
-			{
-				// No views in the tree already exist, so create all views starting from the root view
-				lastExistingViewID = -1;
-				lastExistingViewName = engine.GetDms().GetView(-1).Name;
-				firstNonExistingViewLevel = 0;
-			}
+            return infoResponse.iRet;
+        }
 
-			for (int i = firstNonExistingViewLevel.Value; i < viewNames.Length; ++i)
-			{
-				lastExistingViewID = CreateNewView(viewNames[i], lastExistingViewName);
-				lastExistingViewName = viewNames[i];
-			}
+        private int CreateViews(string[] viewNames)
+        {
+            int? firstNonExistingViewLevel = null;
+            int? lastExistingViewID = null;
+            string lastExistingViewName = null;
 
-			return lastExistingViewID.Value;
-		}
+            for (int i = viewNames.Length - 1; i >= 0; --i)
+            {
+                int? viewID = GetView(viewNames[i]);
+                if (viewID.HasValue)
+                {
+                    lastExistingViewID = viewID;
+                    lastExistingViewName = viewNames[i];
+                    firstNonExistingViewLevel = i + 1;
+                    break;
+                }
+            }
 
-		private void CreateElement(string elementName, string protocolName, string protocolVersion, int viewID,
-			string trendTemplate = "Default", string alarmTemplate = "")
-		{
-			var request = new AddElementMessage
-			{
-				ElementName = elementName,
-				ProtocolName = protocolName,
-				ProtocolVersion = protocolVersion,
-				TrendTemplate = trendTemplate,
-				AlarmTemplate = alarmTemplate,
-				ViewIDs = new int[] { viewID },
-			};
+            if (firstNonExistingViewLevel.HasValue && firstNonExistingViewLevel == viewNames.Length)
+                return lastExistingViewID.Value;
 
-			var dms = engine.GetDms();
-			if (dms.ElementExists(elementName)) //Delete element first if it already exists
-			{
-				var elementRequest = new GetElementByNameMessage(elementName);
-				var elementResponse = engine.SendSLNetSingleResponseMessage(elementRequest);
-				if (!(elementResponse is ElementInfoEventMessage elementInfo))
-					throw new ArgumentException("Unexpected message returned by DataMiner");
+            if (!firstNonExistingViewLevel.HasValue)
+            {
+                // No views in the tree already exist, so create all views starting from the root view
+                lastExistingViewID = -1;
+                lastExistingViewName = engine.GetDms().GetView(-1).Name;
+                firstNonExistingViewLevel = 0;
+            }
 
-				// Remove the element if it exists
-				var deleteRequest = new SetElementStateMessage(elementInfo.DataMinerID, elementInfo.ElementID, Skyline.DataMiner.Net.Messages.ElementState.Deleted, true);
-				engine.SendSLNetMessage(deleteRequest);
-				System.Threading.Thread.Sleep(TimeSpan.FromSeconds(2));
-			}
+            for (int i = firstNonExistingViewLevel.Value; i < viewNames.Length; ++i)
+            {
+                lastExistingViewID = CreateNewView(viewNames[i], lastExistingViewName);
+                lastExistingViewName = viewNames[i];
+            }
 
-			//Verify deletion succeeded
-			for (int i = 0; i < 5; ++i)
-			{
-				if (dms.ElementExists(elementName))
-				{
-					Thread.Sleep(5000);
-				}
-				else
-				{
-					break;
-				}
-			}
+            return lastExistingViewID.Value;
+        }
 
-			//create element
-			engine.SendSLNetSingleResponseMessage(request);
-		}
-	}
+        private void CreateElement(string elementName, string protocolName, string protocolVersion, int viewID,
+            string trendTemplate = "Default", string alarmTemplate = "")
+        {
+            var request = new AddElementMessage
+            {
+                ElementName = elementName,
+                ProtocolName = protocolName,
+                ProtocolVersion = protocolVersion,
+                TrendTemplate = trendTemplate,
+                AlarmTemplate = alarmTemplate,
+                ViewIDs = new int[] { viewID },
+            };
+
+            var dms = engine.GetDms();
+            if (dms.ElementExists(elementName)) //Delete element first if it already exists
+            {
+                var elementRequest = new GetElementByNameMessage(elementName);
+                var elementResponse = engine.SendSLNetSingleResponseMessage(elementRequest);
+                if (!(elementResponse is ElementInfoEventMessage elementInfo))
+                    throw new ArgumentException("Unexpected message returned by DataMiner");
+
+                // Remove the element if it exists
+                var deleteRequest = new SetElementStateMessage(elementInfo.DataMinerID, elementInfo.ElementID, Skyline.DataMiner.Net.Messages.ElementState.Deleted, true);
+                engine.SendSLNetMessage(deleteRequest);
+                System.Threading.Thread.Sleep(TimeSpan.FromSeconds(2));
+            }
+
+            //Verify deletion succeeded
+            for (int i = 0; i < 5; ++i)
+            {
+                if (dms.ElementExists(elementName))
+                {
+                    Thread.Sleep(5000);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            //create element
+            engine.SendSLNetSingleResponseMessage(request);
+        }
+    }
 }
